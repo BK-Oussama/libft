e8166f11edb2da4410b1a2f8175fa8af2ddf2a79
ouboukou <ouboukou@c3r7p9.1337.ma>
2023-12-21 18:05:20 +0100

    hey

--- funct_/ft_substr.c
+++ funct_/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ouboukou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 21:01:26 by ouboukou          #+#    #+#             */
-/*   Updated: 2023/12/19 17:33:54 by ouboukou         ###   ########.fr       */
+/*   Updated: 2023/12/21 16:33:00 by ouboukou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,17 +17,25 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	j;
+	size_t	i;
 	char	*str;
 
+	i = 0;
 	if (s == NULL)
-		return (NULL);
-	if (start > ft_strlen(s))
-		len = 0;
-	if (len > ft_strlen(s + start))
-		len = ft_strlen(s + start);
-	str = malloc(len + 1);
+		return (ft_strdup(""));
+	j = ft_strlen(s);
+	while (i + start < j && i < len)
+		i++;
+	str = malloc(i + 1);
 	if (str == NULL)
-		return (NULL);
-	ft_strlcpy(str, s + start, len + 1);
+		return (0);
+	j = 0;
+	while (i > 0)
+	{
+		str[j++] = s[start++];
+		i--;
+	}
+	str[j] = '\0';
 	return (str);
 }
