64b055635ca3dd3154478ed4e229506de31bac41
ouboukou <ouboukou@c3r10p3.1337.ma>
2023-12-28 19:40:44 +0100

    try again with hope

--- funct_/ft_lstadd_back_bonus.c
+++ funct_/ft_lstadd_back_bonus.c
@@ -16,14 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*ptr;
 
-	// if (*lst == NULL && new == NULL)
-	// 	return ;
+	if (*lst == NULL && new == NULL)
+		return ;
 	if (*lst == NULL)
 	{
 		*lst = new;
 		return ;
 	}
-	new->next = NULL;
 	ptr = ft_lstlast(*lst);
 	ptr->next = new;
 }
